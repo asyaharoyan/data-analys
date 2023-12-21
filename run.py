9d3d97d0a3c79edef06676d9ee08f3e9808d7e6d
asyaharoyan <>
2023-12-21 10:23:13 +0000

    validate the provided data

--- run.py
+++ run.py
@@ -16,16 +16,43 @@ def add_data():
     """
     Ask the user to add the data in the terminal
     """
+    user_input = []
+
+    questions = [
+    "Your gender(W/M/None): ",
+    "Your age: ",
+    "How satisfied are you from our delivery? (1 - 5): ",
+    "Were any missing/damaged goods?(Y/N): ",
+    "Did you contact to our customer service or did you complain through our website?(CS/W): ",
+    "How satisfied are you from our customer service/online services? (1 - 5): ",
+    ]
+
     print('Please answer the questions.')
 
-    your_gender = input("Your gender: \n")
-    your_age = input("Your age: \n")
-    delivery_sat = input("How satisfied are you from our delivery? (1 - 5): \n")
-    missing_goods = input("Were any missing/damaged goods?: \n")
-    contact_method = input("Did you contact to our customer service or did you complain through our website?: \n")
-    service_sat = input("How satisfied are you from our customer service/online services? (1 - 5): \n")
+    for question in questions:
+        answer = input(question)
+        
+        user_input.append(answer)
+        
+    validate_data(user_input)
 
-    print(f"The data you provided is: {your_gender}, {your_age}, {delivery_sat}, {missing_goods}, {contact_method}, {service_sat}\n")
+
+def validate_data(values):
+    """
+    Validates the data provided by the user. 
+    """
+    try:
+        if (values[0].upper() not in ['W', 'M', 'NONE'] 
+        and not values[1].isdigit()
+        and not 1 >= int(values[2]) <= 5 
+        and values[3].upper() not in ['Y', 'N']
+        and values[4].upper() not in ['CS', 'W'] 
+        and not 1 >= int(values[5]) <= 5):
+            print('Invalid input.')
+        else:
+            print('The data has been validated.\n')
+    except ValueError as e:
+        print(f'Invalid data {e}, please try again.\n') 
 
 add_data()
 
